36a02484c0bca0e1e2ed59e8e9313178cd80e5ad
Norcinu <ledbybyson@gmail.com>
2012-05-01 23:31:40 +0100

    Tidied up World Map Editor. Removed SDL.NET References, will use D3D engine.

--- World Map Editor/frmMain.Designer.cs
+++ World Map Editor/frmMain.Designer.cs
@@ -29,26 +29,8 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
-            this.surfaceControl1 = new SdlDotNet.Windows.SurfaceControl();
             this.button1 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.surfaceControl1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // surfaceControl1
-            // 
-            this.surfaceControl1.AccessibleDescription = "SdlDotNet SurfaceControl";
-            this.surfaceControl1.AccessibleName = "SurfaceControl";
-            this.surfaceControl1.AccessibleRole = System.Windows.Forms.AccessibleRole.Graphic;
-            this.surfaceControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.surfaceControl1.Image = ((System.Drawing.Image)(resources.GetObject("surfaceControl1.Image")));
-            this.surfaceControl1.InitialImage = ((System.Drawing.Image)(resources.GetObject("surfaceControl1.InitialImage")));
-            this.surfaceControl1.Location = new System.Drawing.Point(12, 85);
-            this.surfaceControl1.Name = "surfaceControl1";
-            this.surfaceControl1.Size = new System.Drawing.Size(320, 240);
-            this.surfaceControl1.TabIndex = 0;
-            this.surfaceControl1.TabStop = false;
             // 
             // button1
             // 
@@ -66,18 +48,15 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(803, 435);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.surfaceControl1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.surfaceControl1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private SdlDotNet.Windows.SurfaceControl surfaceControl1;
         private System.Windows.Forms.Button button1;
     }
 }
--- World Map Editor/frmMain.cs
+++ World Map Editor/frmMain.cs
@@ -6,51 +6,44 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
-using SdlDotNet.Windows;
-using SdlDotNet.Graphics;
+//using SdlDotNet.Windows;
+//using SdlDotNet.Graphics;
 using System.Threading;
-using SdlDotNet.Core;
+//using SdlDotNet.Core;
 
 namespace World_Map_Editor
 {
     public partial class frmMain : Form
     {
         //private SurfaceControl surfaceControl;
-        private Surface surface;
+        //private Surface surface;
 
         public frmMain()
         {
             InitializeComponent();
-            SdlDotNet.Core.Events.Tick += new EventHandler<TickEventArgs>( Events_Tick );
-            surface = new Surface( this.surfaceControl1.Width, this.surfaceControl1.Height );
+           // SdlDotNet.Core.Events.Tick += new EventHandler<TickEventArgs>( Events_Tick );
+          //  surface = new Surface( this.surfaceControl1.Width, this.surfaceControl1.Height );
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
             
-            Thread thread = new Thread( new ThreadStart( SdlDotNet.Core.Events.Run ) );
-            thread.IsBackground = true;
-            thread.Name = "SDL.NET";
-            thread.Priority = ThreadPriority.Normal;
-            thread.Start();
-        }
-
-        private void Events_Tick(object sender, TickEventArgs e)
-        {
-            surface.Fill( Color.Black );
-            surface.Blit( this.surface );
-            this.UpdateForm();
+            //Thread thread = new Thread( new ThreadStart( SdlDotNet.Core.Events.Run ) );
+            //thread.IsBackground = true;
+            //thread.Name = "SDL.NET";
+            //thread.Priority = ThreadPriority.Normal;
+            //thread.Start();
         }
 
         public void UpdateForm()
         {
-            this.surfaceControl1.Blit( surface );
+            //this.surfaceControl1.Blit( surface );
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            SdlDotNet.Graphics.Primitives.Line ln = new SdlDotNet.Graphics.Primitives.Line( 20, 20, 200, 50 );
-            surface.Draw( ln, Color.Wheat );
+            //SdlDotNet.Graphics.Primitives.Line ln = new SdlDotNet.Graphics.Primitives.Line( 20, 20, 200, 50 );
+            //surface.Draw( ln, Color.Wheat );
             this.UpdateForm();
         }
     }
